75375593643e0f82ffe09d04b72495a4a503eec9
sophiechan <sophiechan@users.noreply.github.com>
2016-10-04 12:33:45 -0400

    Add files via upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,17 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Availabook</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
+        <!-- Styles background-color: #fff; color: #636b6f;-->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-color: #ffd373;
+                
+                color: #06276f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
@@ -75,15 +76,19 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Availabook
                 </div>
-
+<!--
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+-->
+                <div class="content">
+                    <a">Tell your friends when you are available</a>
                 </div>
             </div>
         </div>
